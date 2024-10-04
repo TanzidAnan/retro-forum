a689a0be4e7a2cb6970142a96e55a8728b6f939b
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-04 20:44:20 +0600

    fetch data console e

---
+++ script.js
@@ -0,0 +1,6 @@
+const loadAllPost = async() =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+    const data = await res.json();
+    console.log(data.posts)
+ }
+ loadAllPost()
