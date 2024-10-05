650492a469f5dbb724f254e063f9e45b7c9d4a1e
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-05 11:55:53 +0600

    funcation data load

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const loadAllPost = async() =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
-    const data = await res.json();
-    console.log(data.posts)
- }
- loadAllPost()
+const loadAllPost = async (category) => {
+    console.log(category)
+    if(category){
+        console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`)
+    }
+    else{
+        console.log(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+    }
+    // const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+    // const data = await res.json();
+    // console.log(data.posts)
+}
+loadAllPost();
+
+const handleSearchByCategory = () => {
+    const searchText =document.getElementById('searchPosts').value;
+    console.log(searchText);
+    loadAllPost(searchText)
+}
