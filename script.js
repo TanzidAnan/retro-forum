da9bd6d6ffbea041f49306200c3fc97023481b2c
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-05 15:00:40 +0600

    commit

--- script.js
+++ script.js
@@ -18,7 +18,56 @@ const loadAllPost = async (category) => {
 }
 
 const displayAllPost =(posts) =>{
-    console.log(posts)
+    const postContainer =document.getElementById('post-container');
+    posts.forEach(post =>{
+        // console.log(post)
+        const div =document.createElement('div');
+        div.innerHTML =`
+        <div class="p-6 lg:p-12 flex lg:flex-row flex-col items-center lg:items-start
+      bg-slate-300 rounded-3xl">
+      <div class="indicator ">
+        <span class="indicator-item badge  ${post.isActive ? "bg-green-400":"bg-red-300"}"></span>
+        <div class="avatar">
+          <div class="w-24 rounded-xl">
+            <img src=${post.image} alt="">
+          </div>
+        </div>
+        <div class="space-y-4 w-full">
+          <div class="flex gap-4 *:opacity-60">
+            <p># category</p>
+            <p>Author: ${post.author.name}</p>
+          </div>
+          <h3 class="text-2xl font-bold opacity-70">${post.title}</h3>
+          <p class="opacity-40">${post.description}</p>
+          <hr class="border border-gray-300">
+          <div class="flex justify-between *:font-bold [&>*:not(:last-child)]:opacity-45">
+            <div class="flex gap-4">
+              <div class="space-x-2 flex items-center ">
+                <i class="fa-regular fa-comment-dots"></i>
+                <p>${post.comment_count}</p>
+              </div>
+              <div class="space-x-2 flex items-center">
+                <i class="fa-regular fa-eye"></i>
+                <p>${post.view_count}</p>
+              </div>
+              <div class="space-x-2 flex items-center">
+                <i class="fa-regular fa-eye"></i>
+                <p>${post.posted_time}</p>
+              </div>
+            </div>
+          </div>
+          <div class="opacity-100">
+            <button id="addToList" onclick="markAsRead()" data-post=${JSON.stringify(post)} class="addToList btn btn-circle bg-green-500 btn-sm">
+            <i class="fa-solid fa-landmark text-white"></i>
+           
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>
+        `
+        postContainer.appendChild(div)
+    })
 }
 
 loadAllPost();
