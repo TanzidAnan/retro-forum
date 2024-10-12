e203bf692589a0547cd3c0ad5bbf7aa953dc1ae6
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-12 11:04:23 +0600

    discrapation

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const markAsRead = (description, view_count) => {
     const markAsReadContainer = document.getElementById('markAsReadContainer');
     const div = document.createElement('div');
     div.innerHTML = `
-        <div class="flex justify-between p-2 lg:p-3 bg-white rounded-2xl items-center gap-3">
+        <div class="flex justify-between px-5 lg:p-3 bg-white rounded-2xl items-center gap-3">
             <div class="lg:w-4/5 w-11/12">
                 <p>${description}</p>
                 <div class="lg:w-full w-4/12 flex justify-between gap-5 mt-3">
